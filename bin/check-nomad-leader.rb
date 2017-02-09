e382a7406df3af57e153f4fc4bf3de5880cd2f9b
Jonathan Ballet <jballet@edgelab.ch>
2017-02-09 15:48:55 +0100

    check-nomad-leader: add a check for Nomad leadership

---
+++ bin/check-nomad-leader.rb
@@ -0,0 +1,41 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+#
+#   check-nomad-leader
+#
+# DESCRIPTION:
+#   This plugin checks there's a Nomad server node elected as a leader.
+#
+
+require 'sensu-plugin/check/cli'
+require 'rest-client'
+require 'json'
+
+class CheckNomadLeader < Sensu::Plugin::Check::CLI
+  option :nomad,
+         description: 'Nomad server URL',
+         long: '--nomad SERVER',
+         default: 'http://localhost:4646'
+
+  # Call Nomad api and parse the json response
+  def api_call(endpoint)
+    url = config[:nomad] + endpoint
+    begin
+      response = RestClient.get(url)
+    rescue => e
+      critical "Error #{e.http_code}: #{e.response}"
+    else
+      begin
+        return JSON.parse(response)
+      rescue => e
+        critical "Unable to parse JSON in response: #{e}"
+      end
+    end
+  end
+
+  def run
+    leader = api_call '/v1/status/leader'
+    ok "Nomad leader is #{leader}"
+  end
+end
