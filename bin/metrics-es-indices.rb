afb7a27dbe6df4208dd9b8f5f68ead261c0a4baf
Jonathan Ballet <jballet@edgelab.ch>
2017-07-31 13:39:05 +0200

    elasticsearch: get metrics for Elasticsearch indices

---
+++ bin/metrics-es-indices.rb
@@ -0,0 +1,113 @@
+#! /usr/bin/env ruby
+#
+#   metrics-es-indices
+#
+# DESCRIPTION:
+#   This plugin uses the ES API to collect index-related metrics, producing a
+#   JSON document which is outputted to STDOUT. An exit status of 0 indicates
+#   the plugin has successfully collected and produced metrics.
+#
+# OUTPUT:
+#   metric data
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   gem: rest-client
+#
+# USAGE:
+#   #YELLOW
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2017 EdgeLaboratories.
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'sensu-plugin/metric/cli'
+require 'rest-client'
+require 'json'
+require 'base64'
+
+#
+# ES Indices Metrics
+#
+class ESIndicesMetrics < Sensu::Plugin::Metric::CLI::Graphite
+  option :scheme,
+         description: 'Metric naming scheme, text to prepend to metric',
+         short: '-s SCHEME',
+         long: '--scheme SCHEME',
+         default: "#{Socket.gethostname}.elasticsearch.index"
+
+  option :host,
+         description: 'Elasticsearch host',
+         short: '-h HOST',
+         long: '--host HOST',
+         default: 'localhost'
+
+  option :port,
+         description: 'Elasticsearch port',
+         short: '-p PORT',
+         long: '--port PORT',
+         proc: proc(&:to_i),
+         default: 9200
+
+  option :timeout,
+         description: 'Sets the connection timeout for REST client',
+         short: '-t SECS',
+         long: '--timeout SECS',
+         proc: proc(&:to_i),
+         default: 30
+
+  option :user,
+         description: 'Elasticsearch User',
+         short: '-u USER',
+         long: '--user USER'
+
+  option :password,
+         description: 'Elasticsearch Password',
+         short: '-P PASS',
+         long: '--password PASS'
+
+  option :https,
+         description: 'Enables HTTPS',
+         short: '-e',
+         long: '--https'
+
+  def get_es_resource(resource)
+    headers = {}
+    if config[:user] && config[:password]
+      auth = 'Basic ' + Base64.strict_encode64("#{config[:user]}:#{config[:password]}").chomp
+      headers = { 'Authorization' => auth }
+    end
+
+    protocol = if config[:https]
+                 'https'
+               else
+                 'http'
+               end
+
+    r = RestClient::Resource.new("#{protocol}://#{config[:host]}:#{config[:port]}#{resource}", timeout: config[:timeout], headers: headers)
+    JSON.parse(r.get)
+  rescue Errno::ECONNREFUSED
+    warning 'Connection refused'
+  rescue RestClient::RequestTimeout
+    warning 'Connection timed out'
+  end
+
+  def run
+    indices_stats = get_es_resource('/_stats/docs,store')
+
+    indices_stats['indices'].each do |name, index|
+      name = name.tr('.', '_')
+      output(config[:scheme] + '.' + name + '.documents.total', index['total']['docs']['count'])
+      output(config[:scheme] + '.' + name + '.documents.deleted', index['total']['docs']['deleted'])
+      output(config[:scheme] + '.' + name + '.disk_size.bytes', index['total']['store']['size_in_bytes'])
+    end
+    ok
+  end
+end
